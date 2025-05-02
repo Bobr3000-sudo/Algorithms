35bcaf8542f2440102f42efb7802bb694871de4e
Bobr3000-sudo <kostianty1@gmail.com>
2025-05-02 22:27:15 +0300

    Floid-W algorithm not finished

--- Floyd-Warshall  algorithm/code.cpp
+++ Floyd-Warshall  algorithm/code.cpp
@@ -9,12 +9,27 @@ int main()
     std::cout << "size of matrix: ";
     std::cin >> n;
     std::cout << "matrix: " << std::endl;
-    std::vector<std::vector<int>> x(n, std::vector<int>(n));
+    std::vector<std::vector<int>> d(n, std::vector<int>(n));
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            std::cin >> x[i][j];
+            std::cin >> d[i][j];
+        }
+    }
+    std::vector<std::vector<int>> d1(n, std::vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if(i == j)
+            {
+                d1[i][j] = d[i][j];
+            }
+            else
+            {
+                if(d[i][j] )
+            }
         }
     }
 
