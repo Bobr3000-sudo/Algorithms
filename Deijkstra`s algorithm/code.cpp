72f6b71673a72d4d8d878376f744675cdc4bd43a
Bobr3000-sudo <kostianty1@gmail.com>
2025-05-04 17:49:59 +0300

    Deijksta s algorithm

--- Deijkstra`s algorithm/code.cpp
+++ Deijkstra`s algorithm/code.cpp
@@ -1,23 +1,20 @@
 #include <iostream>
 #include <vector>
-#include <limits>
+#include <climits>
 
-const int i = std::numeric_limits<int>::max();
+const int inf = std::numeric_limits<int>::max();
 
 int main()
 {
     int n;
     std::cout << "Numder: ";
     std::cin >> n;
-    std::vector<std::vector<int>> graph(n, std::vector<int>(n, i));
+    std::vector<std::vector<int>> graph(n, std::vector<int>(n, inf));
     std::vector <int> distance(n);
     std::vector <bool> visited(n, false);
     for(int i = 0; i < n; i++)
     {
-        for(int j = 0; j < n; j++)
-        {
-            std::cin >> graph[i][j];
-        }
+        distance[i] = inf;
     }
     for(int i = 0; i < n; i++)
     {
@@ -26,5 +23,44 @@ int main()
             std::cin >> graph[i][j];
         }
     }
+    bool check = false;
+    visited[0] = true;
+    while(check)
+    {
+        int position = inf;
+        int min = INT_MAX;
+        for(int i = 0; i < n; i++)
+        {
+            if (!visited[i] && graph[position][i] < min)
+            {
+                min = distance[i];
+                position = i;
+            }
+        }
+        if(position == inf)
+        {
+            check = true;
+            break;
+        }
+        else
+        {
+            visited[position] = true;
+            for(int i = 0; i < n; i++)
+            {
+                if(graph[position][i] != inf && !visited[i])
+                {
+                    if(distance[position] + graph[position][i] < distance[i])
+                    {
+                        distance[i] = distance[position] + graph[position][i];
+                    }
+                }
+            }
+        }
+    }
+    for(int i = 0; i < n; i++)
+    {
+        std::cout << i << " " << distance[i] << std::endl;
+    }
+
     return 0;
 }
